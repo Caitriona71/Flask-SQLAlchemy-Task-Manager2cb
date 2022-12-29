acd0f494ffb9c335290d222fd3ddd84bb7b60f5d
Caitriona71 <caitrionaboyle@ymail.com>
2022-12-29 13:51:35 +0000

    Create categories

--- taskmanager/models.py
+++ taskmanager/models.py
@@ -2,6 +2,7 @@ from taskmanager import db
 
 
 class Category(db.Model):
+
     # schema for the Category model
     id = db.Column(db.Integer, primary_key=True)
     category_name = db.Column(db.String(25), unique=True, nullable=False)
@@ -9,16 +10,20 @@ class Category(db.Model):
 
     def __repr__(self):
         # __repr__ to represent itself in the form of a string
-        return "#{0} - Task: {1} ǀ Urgent: {2}".format(
-            self.id, self.task_name, self.is_urgent
-        )
+        return self.category_name
 
 
 class Task(db.Model):
     # schema for the Task model
-     id = db.Column(db.Integer, primary_key=True)
-     task_name = db.Column(db.String(50), unique=True, nullable=False)
-     task_description = db.Column(db.Text, nullable=False)
-     is_urgent = db.Column(db.Boolean, default=False, nullable=False)
-     due_date = db.Column(db.Date, nullable=False) 
-     category_id = db.Column(db.Integer, db.ForeignKey("category.id", ondelete="CASCADE"), nullable=False)
+    id = db.Column(db.Integer, primary_key=True)
+    task_name = db.Column(db.String(50), unique=True, nullable=False)
+    task_description = db.Column(db.Text, nullable=False)
+    is_urgent = db.Column(db.Boolean, default=False, nullable=False)
+    due_date = db.Column(db.Date, nullable=False)
+    category_id = db.Column(db.Integer, db.ForeignKey("category.id", ondelete="CASCADE"), nullable=False)
+
+    def __repr__(self):
+        # __repr__ to represent itself in the form of a string
+        return "#{0} - Task: {1} | Urgent: {2}".format(
+            self.id, self.task_name, self.is_urgent
+        )
